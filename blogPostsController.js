d0d2dead45ee038cc8d85a453b11ab0ec396c49c
LongTR11 <sapper11.tl@gmail.com>
2018-10-02 14:16:22 -0400

    first complete commit

--- blogPostsController.js
+++ blogPostsController.js
@@ -1,29 +1,58 @@
-const {BlogPosts} = require('../models')
+const { BlogPosts } = require('../models')
 
 // getAll
-BlogPost.find().then(function(pets) {
-    res.render('blog-posts', );
-});
-
+function(id) {
+    // if id passed in, retrieve single post,
+    // otherwise send all posts.
+    if (id !== null) {
+        return this.posts.find(post => post.id === id);
+    }
+    // return posts sorted (descending) by
+    // publish date
+    return this.posts.sort(function (a, b) {
+        return b.publishDate - a.publishDate
+    });
+}
 //post
-
+function(title, content, author, publishDate) {
+    const post = {
+        id: uuid.v4(),
+        title: title,
+        content: content,
+        author: author,
+        publishDate: publishDate || Date.now()
+    };
+    this.posts.push(post);
+    return post;
+}
 //put
-function update(req, res) {
-    res.render('/blog-posts/:id');
+function(updatedPost) {
+    const { id } = updatedPost;
+    const postIndex = this.posts.findIndex(
+        post => post.id === updatedPost.id);
+    if (postIndex === -1) {
+        throw new StorageException(
+            `Can't update item \`${id}\` because doesn't exist.`)
+    }
+    this.posts[postIndex] = Object.assign(
+        this.posts[postIndex], updatedPost);
+    return this.posts[postIndex];
 }
 
 // delete
-function destroy(req, res) {
-    Pet.findByIdAndRemove(req.params.id).then(function(blogPost) {
-        res.redirect('/blog-posts/:id');
-    })
+function(id) {
+    const postIndex = this.posts.findIndex(
+        post => post.id === id);
+    if (postIndex > -1) {
+        this.posts.splice(postIndex, 1);
+    }
 }
 
 const BlogPostsController = {
     getAll: getAll,
     post: post,
     put: put,
-    destroy: destroy
+    delete: delete;
 };
 
 module.export = BlogPostsController;
--- routes.js
+++ routes.js
@@ -1,10 +1,11 @@
 
 const express = require('express');
 const router = express.Router();
-const {BlogPosts} = require('./models');
-const 
+const {blogPostsController} = require('./blogPostsController');
 
 router.get('/blog-posts', blogPostsController.getAll);
 router.post('/blog-posts', BlogPosts.create()); 
 router.put('/blog-posts/:id', BlogPosts.update());
 router.delete('/blog-posts/:id', BlogPosts.delete());
+
+router.get('/blog-posts', blogPostsController.getAll);
